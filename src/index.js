dd7445ccdac2e1a07d13445854848ac7267132e9
Jorge Carrion <jorge_28cs@hotmail.com>
2022-07-09 00:45:20 -0500

    added zingchart

--- src/index.js
+++ src/index.js
@@ -1,24 +1,50 @@
 const W3CWebSocket = require('websocket').w3cwebsocket;
 const client = new W3CWebSocket('ws://localhost:1880/ws/plc', 'echo-protocol');
+require('zingchart');
 
 const gauge_01 = document.getElementById('gauge_01');
-console.log(gauge_01)
-client.onerror = function() {
-    console.log('Connection Error');
+client.onerror = function () {
+  console.log('Connection Error');
 };
 
-client.onopen = function() {
-    console.log('WebSocket Client Connected');
+client.onopen = function () {
+  console.log('WebSocket Client Connected');
 };
 
-client.onclose = function() {
-    console.log('echo-protocol Client Closed');
+client.onclose = function () {
+  console.log('echo-protocol Client Closed');
 };
 
-client.onmessage = function(e) {
+const gauge_01_config = (value, min, max) => {
+  return {
+    type: 'gauge',
+    series: [
+      {
+        values: [value],
+      },
+    ],
+    scaleR: {
+      minValue: min,
+      maxValue: max,
+    },
+  };
+};
+
+zingchart.render({
+  id: 'gauge_01',
+  data: gauge_01_config(0, 0, 100),
+  height: '400px',
+  width: '400px',
+});
+
+client.onmessage = function (e) {
   const payload = JSON.parse(e.data);
-  console.log(payload);
-  if (payload.gauge_01){
-    gauge_01.innerHTML = `${payload.gauge_01.current_value} PSI`;
+  if (payload.gauge_01) {
+    zingchart.render({
+      id: 'gauge_01',
+      data: gauge_01_config(payload.gauge_01, 0, 100),
+      height: '400px',
+      width: '400px',
+    });
   }
 };
